fdafe99ac5b762197c1962d24db31d87455701bc
Jerry Spice <JoshlandiaOfficial@gmail.com>
2024-03-14 22:23:46 -0400

    Adding a backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "JoshlandiaWebsite": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "JoshlandiaWebsite": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3joshlandiawebsitestorage2df54ab9": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
