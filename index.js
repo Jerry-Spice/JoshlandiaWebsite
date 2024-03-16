8e6433d0b91a05e17dae8bd2a9084c354bbe238c
Jerry Spice <JoshlandiaOfficial@gmail.com>
2024-03-16 00:56:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import Amplify  from './amplify';
 import config from './amplify.config';
 Amplify.configure(config);
 
-// import { uploadData } from 'aws-amplify/storage';
-//
-// try {
-//   const result = await uploadData({
-//     key: filename,
-//     data: file
-//   }).result;
-//   console.log('Succeeded: ', result);
-// } catch (error) {
-//   console.log('Error : ', error);
-// }
+import { uploadData } from './amplify/backend/storage';
+
+try {
+  const result = await uploadData({
+    key: filename,
+    data: file
+  }).result;
+  console.log('Succeeded: ', result);
+} catch (error) {
+  console.log('Error : ', error);
+}
