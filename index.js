42646d288dc630d3209434d8d7f9bafa7c99216c
Jerry Spice <JoshlandiaOfficial@gmail.com>
2024-03-16 00:20:28 -0400

    wsfsghjfgfdsa

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-console.log(":)");
-
-import { Amplify } from './aws-amplify';
-
-import amplifyconfig from './amplifyconfiguration.json';
-
-
-Amplify.configure(amplifyconfig);
-
-import { list } from './aws-amplify/storage';
-
-
-try {
-
-  const result = await list({
-
-    prefix: 'files/'
-
-  });
-  console.log(result);
-  var listOfFiles = document.getElementById("listEl");
-
-  listOfFiles.innerHTML = result[0];
-} catch (error) {
-  console.log("Uh oh");
-  console.log(error);
-
-}
+import { Amplify } from 'aws-amplify';
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
